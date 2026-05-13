dbad97c6da1de058018fbf2c45fe71a957d43dfe
Tom Wright <tom@inflatablecookie.com>
2026-05-13 14:40:59 +0100

    Adopt new error log list page and raise Effigy minimum
    - Switch admin error log page to `ErrorLogListPage`
    - Add `minimum_effigy_version = "0.7.0"` to app manifests

--- acme-admin/src/routes/(app)/system/errors/+page.svelte
+++ acme-admin/src/routes/(app)/system/errors/+page.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import { page } from "$app/stores";
-  import { buildQueryString, parseQueryParams, type QueryParams } from "@decodelabs/underlay/client/query";
   import { ErrorLogList } from "$lib/lists";
-
-  const currentQuery = $derived(parseQueryParams($page.url.searchParams));
-
-  function updateUrl(nextQuery: QueryParams) {
-    const url = new URL($page.url);
-    url.search = buildQueryString(nextQuery);
-    goto(url.toString(), { replaceState: true, keepFocus: true });
-  }
 </script>
 
-<ErrorLogList query={currentQuery} onQueryChange={updateUrl} />
+<ErrorLogList />
