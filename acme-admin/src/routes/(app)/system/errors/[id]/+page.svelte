6cd7e3cf57321996aab7f73b493c82710c7f9868
Tom Wright <tom@inflatablecookie.com>
2026-05-18 08:25:39 +0100

    Adopt shared error log detail page

---
+++ acme-admin/src/routes/(app)/system/errors/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { adminCommands } from "@api-client";
+  import { ErrorLogDetailPage } from "@decodelabs/underlay/templates";
+
+  const errorLogId = $derived.by(() => $page.params.id ?? "");
+
+  async function dataLoader(id: string, fetch: typeof globalThis.fetch, token: string) {
+    return await adminCommands.getErrorLog(id, fetch, token);
+  }
+</script>
+
+<ErrorLogDetailPage
+  id={errorLogId}
+  {dataLoader}
+/>
